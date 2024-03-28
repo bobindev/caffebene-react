e8e1b10940ef48e41325d42f63f5c23d7e15c3cf
boburasatullaev91@gmail.com <boburasatullaev91@gmail.com>
2024-03-28 12:10:47 +0900

    fix: develop burak screen based components

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -2,44 +2,58 @@ import React from 'react';
 import '../css/app.css';
 import { Box, Button, Container, Stack, Typography } from '@mui/material';
 import { Link, Route, Switch } from 'react-router-dom';
-import { About } from './screens/About';
-import { Users } from './screens/Users';
+import { HomePage } from './screens/homePage';
+import { ProductsPage } from './screens/productsPage';
+import { OrdersPage } from './screens/ordersPage';
+import { UserPage } from './screens/userPage';
+import { HelpPage } from './screens/helpPage';
 
 function App() {
-  return  <div>
-  <nav>
-    <ul>
-      <li>
-        <Link to="/">Home</Link>
-      </li>
-      <li>
-        <Link to="/about">About</Link>
-      </li>
-      <li>
-        <Link to="/users">Users</Link>
-      </li>
-    </ul>
-  </nav>
+  return <div>
+    <nav>
+      <ul>
+        <li>
+          <Link to="/">HopePage</Link>
+        </li>
+        <li>
+          <Link to="/products">ProductsPage</Link>
+        </li>
+        <li>
+          <Link to="/orders">OrdersPage</Link>
+        </li>
+        <li>
+          <Link to="/member-page">UserPage</Link>
+        </li>
+        <li>
+          <Link to="/help">HelpPage</Link>
+        </li>
 
-  {/* A <Switch> looks through its children <Route>s and
+      </ul>
+    </nav>
+
+    {/* A <Switch> looks through its children <Route>s and
       renders the first one that matches the current URL. */}
-  <Switch>
-    <Route path="/about">
-      <About />
-    </Route>
-    <Route path="/users">
-      <Users />
-    </Route>
-    <Route path="/">
-      <Home />
-    </Route>
-  </Switch>
-</div>
+    <Switch>
+      <Route path="/products">
+        <ProductsPage />
+      </Route>
+      <Route path="/orders">
+        <OrdersPage />
+      </Route>
+      <Route path="/member-page">
+        <UserPage/>
+      </Route>
+      <Route path="/help">
+        <HelpPage />
+      </Route>
+      <Route path="/">
+        <HomePage />
+      </Route>
+    </Switch>
+  </div>
 }
 
-function Home() {
-  return <Container>Home</Container>;
-}
+
 
 
 
