02bd4185137940afccfb78557982f9d478fa8282
boburasatullaev91@gmail.com <boburasatullaev91@gmail.com>
2024-03-29 20:53:20 +0900

    feat: develop header and footer part one

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -10,10 +10,11 @@ import { HelpPage } from './screens/helpPage';
 import { HomeNavbar } from './components/headers/HomeNavbar';
 import { OtherNavbar } from './components/headers/OtherNavbar';
 import { Footer } from './components/footer';
+import "../css/app.css";
+import "../css/navbar.css";
 
 function App() {
 const location = useLocation();
-console.log("location:", location);
 
   return( 
   <>
