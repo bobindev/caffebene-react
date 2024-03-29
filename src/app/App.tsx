fdb2278c479b3bfc22cd0b2ad4f5d2fd0af532f0
boburasatullaev91@gmail.com <boburasatullaev91@gmail.com>
2024-03-29 16:59:01 +0900

    fix: orgonize header and footer components

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,38 +1,24 @@
 import React from 'react';
 import '../css/app.css';
 import { Box, Button, Container, Stack, Typography } from '@mui/material';
-import { Link, Route, Switch } from 'react-router-dom';
+import { Link, Route, Switch, useLocation } from 'react-router-dom';
 import { HomePage } from './screens/homePage';
 import { ProductsPage } from './screens/productsPage';
 import { OrdersPage } from './screens/ordersPage';
 import { UserPage } from './screens/userPage';
 import { HelpPage } from './screens/helpPage';
+import { HomeNavbar } from './components/headers/HomeNavbar';
+import { OtherNavbar } from './components/headers/OtherNavbar';
+import { Footer } from './components/footer';
 
 function App() {
-  return <div>
-    <nav>
-      <ul>
-        <li>
-          <Link to="/">HopePage</Link>
-        </li>
-        <li>
-          <Link to="/products">ProductsPage</Link>
-        </li>
-        <li>
-          <Link to="/orders">OrdersPage</Link>
-        </li>
-        <li>
-          <Link to="/member-page">UserPage</Link>
-        </li>
-        <li>
-          <Link to="/help">HelpPage</Link>
-        </li>
+const location = useLocation();
+console.log("location:", location);
 
-      </ul>
-    </nav>
+  return( 
+  <>
 
-    {/* A <Switch> looks through its children <Route>s and
-      renders the first one that matches the current URL. */}
+    {location.pathname === "/" ? <HomeNavbar/> : <OtherNavbar/>}
     <Switch>
       <Route path="/products">
         <ProductsPage />
@@ -50,7 +36,9 @@ function App() {
         <HomePage />
       </Route>
     </Switch>
-  </div>
+    <Footer/>
+  </>
+  );
 }
 
 
