2f927370024000d4bb8343c85468f9e0d858ac8b
boburasatullaev91@gmail.com <boburasatullaev91@gmail.com>
2024-04-02 05:37:14 +0900

    feat: develop activeUsers sectional components

--- src/app/screens/homePage/ActiveUsers.tsx
+++ src/app/screens/homePage/ActiveUsers.tsx
@@ -1,10 +1,55 @@
 import React from "react";
-import { Container } from "@mui/material";
+import { Box, Container, Stack } from "@mui/material";
+import Card from '@mui/joy/Card';
+import { CssVarsProvider, Typography } from "@mui/joy";
+import CardOverflow from "@mui/joy/CardOverflow";
+import AspectRatio from "@mui/joy/AspectRatio";
+import { Css } from "@mui/icons-material";
 
-export default function  ActiveUsers() {
+const activeUsers = [
+    { memberNick: "Martin", imagePath: "/img/martin.webp" },
+    { memberNick: "Justin", imagePath: "/img/justin.webp" },
+    { memberNick: "Rose", imagePath: "/img/rose.webp" },
+    { memberNick: "Nusret", imagePath: "/img/nusret.webp" }
+];
+
+
+export default function ActiveUsers() {
     return (
-        <div>
-            <Container>ActiveUsers</Container>
-        </div>
+        <div className={"active-users-frame"}>
+            <Container>
+                <Stack className={"main"}>
+                    <Box className={"category-title"}>Active Users</Box>
+                    <Stack className={"card-frame"}>
+                        <CssVarsProvider>
+                            {activeUsers.length !== 0 ? (
+                                activeUsers.map((ele, index) => {
+                                    return (
+                                        <Card key={index} variant="outlined" className={"card"}>
+                                            <CardOverflow>
+                                                <AspectRatio ratio="1">
+                                                    <img src={ele.imagePath} alt="" />
+                                                </AspectRatio>
+                                            </CardOverflow>
+
+                                            <CardOverflow variant="soft" className="member-nickname">
+                                                <Stack className="info">
+                                                    <Typography className={"title"}>
+                                                        {ele.memberNick}
+                                                    </Typography>
+                                                </Stack>
+                                            </CardOverflow>
+                                        </Card>
+                                    );
+                                })
+                            ) : (
+                                <Box className="no-data">No Active Users! </Box>
+                            )
+                            }
+                        </CssVarsProvider>
+                    </Stack>
+                </Stack>
+            </Container>
+        </div >
     );
 }
--- src/app/screens/homePage/NewDishes.tsx
+++ src/app/screens/homePage/NewDishes.tsx
@@ -5,8 +5,8 @@ import Card from '@mui/joy/Card';
 import Typography from '@mui/joy/Typography';
 import CardOverflow from "@mui/joy/CardOverflow";
 import VisibilityIcon from "@mui/icons-material/Visibility";
-import AspectRatio  from "@mui/joy/AspectRatio";
-import  Divider  from "../../components/divider";
+import AspectRatio from "@mui/joy/AspectRatio";
+import Divider from "../../components/divider";
 
 
 const newDishes = [
@@ -14,7 +14,7 @@ const newDishes = [
     { productName: "Kebab", imagePath: "/img/kebab-fresh.webp" },
     { productName: "Kebab", imagePath: "/img/kebab.webp" },
     { productName: "Lavash", imagePath: "/img/lavash.webp" }
-]
+];
 
 export default function PopularDishes() {
     return (
@@ -25,44 +25,44 @@ export default function PopularDishes() {
                         Fresh Menu</Box>
                     <Stack className="card-frame">
                         <CssVarsProvider>
-                            {newDishes.length !==0 ? (
-                              newDishes.map((ele, index) => {
-                                return (
-                                    <Card key={index} variant="outlined" className={"card"}>
-                                        <CardOverflow>
-                                            <div className="product-sale">Normal Size</div>
-                                            <AspectRatio ratio="1">
-                                                <img src={ele.imagePath} alt="" />
-                                            </AspectRatio>
-                                        </CardOverflow>
+                            {newDishes.length !== 0 ? (
+                                newDishes.map((ele, index) => {
+                                    return (
+                                        <Card key={index} variant="outlined" className={"card"}>
+                                            <CardOverflow>
+                                                <div className="product-sale">Normal Size</div>
+                                                <AspectRatio ratio="1">
+                                                    <img src={ele.imagePath} alt="" />
+                                                </AspectRatio>
+                                            </CardOverflow>
 
-                                        <CardOverflow variant="soft" className="product-detail">
-                                            <Stack className="info">
-                                                <Stack flexDirection={"row"}>
-                                                    <Typography className = {"title"}>
-                                                        {ele.productName}
-                                                    </Typography>
-                                                    <Divider width="2" height="24" bg="#d9d9d9"/>
-                                                    <Typography className = {"price"}>
-                                                        $12
-                                                    </Typography>
+                                            <CardOverflow variant="soft" className="product-detail">
+                                                <Stack className="info">
+                                                    <Stack flexDirection={"row"}>
+                                                        <Typography className={"title"}>
+                                                            {ele.productName}
+                                                        </Typography>
+                                                        <Divider width="2" height="24" bg="#d9d9d9" />
+                                                        <Typography className={"price"}>
+                                                            $12
+                                                        </Typography>
+                                                    </Stack>
+                                                    <Stack>
+                                                        <Typography className={"views"}>
+                                                            20
+                                                            <VisibilityIcon
+                                                                sx={{ fontSize: 20, marginLeft: "5px" }}
+                                                            />
+                                                        </Typography>
+                                                    </Stack>
                                                 </Stack>
-                                                <Stack>
-                                                <Typography className = {"views"}>
-                                                        20
-                                                        <VisibilityIcon
-                                                        sx={{fontSize:20, marginLeft: "5px"}}
-                                                        />
-                                                    </Typography>
-                                                </Stack>
-                                            </Stack>
-                                        </CardOverflow>
-                                    </Card>
-                                );
-                            })
-                            ): (
+                                            </CardOverflow>
+                                        </Card>
+                                    );
+                                })
+                            ) : (
                                 <Box className="no-data">New products are not available! </Box>
-                            
+
                             )}
                         </CssVarsProvider>
                     </Stack>
