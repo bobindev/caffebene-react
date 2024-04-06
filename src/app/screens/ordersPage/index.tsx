1d0c75688aaafe6b978e634b2b7cef47183f000c
boburasatullaev91@gmail.com <boburasatullaev91@gmail.com>
2024-04-06 10:07:56 +0900

    feat: publish orderPage screen component

--- src/app/screens/ordersPage/index.tsx
+++ src/app/screens/ordersPage/index.tsx
@@ -1,5 +1,114 @@
-import { Container } from "@mui/material";
+import React, { SyntheticEvent, useState } from "react";
+import { Box, Container, Divider, Stack } from "@mui/material";
+import Tab from '@mui/material/Tab';
+import TabContext from '@mui/lab/TabContext';
+import Tabs from '@mui/material/Tabs';
+import LocationOnIcon from "@mui/icons-material/LocationOn";
+import PausedOrders from "./PausedOrders";
+import ProcessOrders from "./ProcessOrders";
+import FinishedOrders from "./FinishedOrders";
+import "../../../css/order.css";
 
 export default function OrdersPage() {
-    return <Container>OrdersPage</Container>;
+
+  const [value, setValue] = useState("1");
+
+  const handleChange = (e: SyntheticEvent, newValue: string) => {
+    setValue(newValue);
   }
+
+  return (
+    <div className="order-page">
+      <Container className="order-container">
+        <Stack className="order-left">
+          <TabContext value={value}>
+            <Box className="order-nav-frame">
+              <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
+                <Tabs
+                  value={value}
+                  onChange={handleChange}
+                  aria-label="basic tabs example"
+                  className="table-list">
+                  <Tab label="PAUSED ORDER" value={"1"} />
+                  <Tab label="PROCESS ORDER" value={"2"} />
+                  <Tab label="FINISHED ORDER" value={"3"} />
+                </Tabs>
+              </Box>
+            </Box>
+            <Stack className="oreder-main-context">
+              <PausedOrders/>
+              <ProcessOrders/>
+              <FinishedOrders/>
+            </Stack>
+          </TabContext>
+        </Stack>
+
+        <Stack className="order-right">
+          <Box className="order-info-box">
+            <Box className="member-box">
+              <div className="order-user-img">
+                <img src="/icons/default-user.svg" alt="" className="order-user-avatar" />
+                <div className="order-user-icon-box">
+                  <img src="/icons/user-badge.svg" alt="" className="order-user-prof-img" />
+                </div>
+              </div>
+              <span className="order-user-name">Martin</span>
+              <span className="order-user-prof">USER</span>
+            </Box>
+            <div className="liner"></div>
+            <Box className="liner" >
+
+            </Box>
+            <Box className="order-user-address">
+              <div style={{ display: "flex" }} className="spec-address-txt">
+                <LocationOnIcon /> Do not exist
+              </div>
+            </Box>
+          </Box>
+          <div className="order-pay-box">
+
+            <div className="first-row">
+              <div className="card-number">
+                <div className="input-field">
+                  <input type="text" placeholder="Card number" />
+                </div>
+              </div>
+            </div>
+
+            <div className="second-row">
+              <div className="ex-date">
+                <div className="input-field">
+                  <input type="text" placeholder="Expired date" />
+                </div>
+              </div>
+              <div className="cvv">
+                <div className="input-field">
+                  <input type="text" placeholder="CVV" />
+                </div>
+              </div>
+            </div>
+
+            <div className="third-row">
+              <div className="owner">
+                <div className="input-field">
+                  <input type="text" placeholder="Full Name" />
+                </div>
+              </div>
+            </div>
+
+            <div className="card-icon">
+              <img src="/icons/western-card.svg" alt="" className="card-icon-img" />
+              <img src="/icons/master-card.svg" alt="" className="card-icon-img" />
+              <img src="/icons/paypal-card.svg" alt="" className="card-icon-img" />
+              <img src="/icons/visa-card.svg" alt="" className="card-icon-img" />
+            </div>
+
+          </div>
+
+        </Stack>
+      </Container>
+    </div>
+
+  )
+
+}
